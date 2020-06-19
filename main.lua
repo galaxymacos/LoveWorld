bd4fcefe6874f98ae1e088a04c01bc8a05c38f6c
Xun Ruan <galaxymacos@gmail.com>
2020-06-19 02:58:18 -0400

    Love Lua Syntax Explore

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+message = 0
+
+testScores = {}
+testScores[1] = 95
+testScores[2] = 87
+testScores[3] = 98
+table.insert(testScores, 100)
+
+for i,v in ipairs(testScores) do
+  message = message + v
+end
+
+
 function love.draw()
-  love.graphics.print("Hello World!")
+  love.graphics.setFont(love.graphics.newFont(50))
+  love.graphics.print(message)
 end
